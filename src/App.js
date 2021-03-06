f0fc74d8e0c98d1edd36b7a341e671f923346bf9
Gavin Crowley <gavincrowley@gmail.com>
2021-03-06 10:21:25 +0400

    inital vanilla commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
+import { InlineForm } from './components/molecules/InlineForm/InlineForm'
 import BradHome from './templates/BradHome'
 import VanillaReactHome from './templates/VanillaReactHome'
 
 const App = () => {
   return (
     <div className="App">
-      {/* <BradHome /> */}
-      <VanillaReactHome />
+      <BradHome />
     </div>
   )
 }
